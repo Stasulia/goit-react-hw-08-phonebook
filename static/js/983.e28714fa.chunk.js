6b93a15461b16a86014d0b0da97bc931d0d96acc
Stasulia <134652473+Stasulia@users.noreply.github.com>
2024-01-30 13:27:55 +0000

    Deploying to gh-pages from @ Stasulia/goit-react-hw-08-phonebook@23aad9ef2a61d802c4e9fa38e3120e54f1550834 🚀

---
+++ static/js/983.e28714fa.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,c){c.r(t);var s=c(1087),a=c(184);t.default=function(){return(0,a.jsxs)("div",{className:"home",children:[(0,a.jsx)("h1",{className:"title",children:"Welcome to Phonebook App"}),(0,a.jsx)("button",{children:(0,a.jsx)(s.OL,{className:"link",to:"/contacts",children:"Start"})})]})}}}]);
+//# sourceMappingURL=983.e28714fa.chunk.js.map
